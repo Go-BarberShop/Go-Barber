3ff2d809b3f34686037a114b65a01e8159e7b850
Carlos Eduardo B. S. Araujo <cadu29bahia@gmail.com>
2024-08-06 23:20:22 -0300

    Spring Initalizer

---
+++ src/main/java/br/edu/ufape/gobarber/GoBarberApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.gobarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoBarberApplication.class, args);
+	}
+
+}
