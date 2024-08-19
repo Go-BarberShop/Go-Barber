519b9b37bb72f016eb9f336ff7365c42e953b584
Lucas Galdêncio Barbosa <lucasgaldencio77@gmail.com>
2024-08-19 12:45:26 -0300

    Correção rota Product.

--- src/main/java/br/edu/ufape/gobarber/controller/ProductController.java
+++ src/main/java/br/edu/ufape/gobarber/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping
+@RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
 
