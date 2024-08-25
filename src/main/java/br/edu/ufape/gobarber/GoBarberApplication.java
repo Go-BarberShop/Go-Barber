bad68dd400dfa0cf730f0ecac08136ae9c031a9a
Carlos Eduardo B. S. Araujo <cadu29bahia@gmail.com>
2024-08-25 15:34:56 -0300

    Implementação do logout no back

--- src/main/java/br/edu/ufape/gobarber/GoBarberApplication.java
+++ src/main/java/br/edu/ufape/gobarber/GoBarberApplication.java
@@ -3,9 +3,11 @@ package br.edu.ufape.gobarber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class GoBarberApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/edu/ufape/gobarber/controller/auth/AuthController.java
+++ src/main/java/br/edu/ufape/gobarber/controller/auth/AuthController.java
@@ -11,11 +11,9 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,7 +42,6 @@ public class AuthController {
             User validatedUser = (User) authentication.getPrincipal();
 
             String jwt = tokenService.gerarTokenJwt(validatedUser);
-            jwt = jwt.replace(jwt.substring(0, 7), "");
             String role = validatedUser.getRole().getAuthority();
             role = role.replace(role.substring(0, 5), "");
 
@@ -59,6 +56,13 @@ public class AuthController {
         } catch (Exception e) {
             return new ResponseEntity<>(Map.of("error", "Falha de Autenticação, Tente Novamente!"), HttpStatus.FORBIDDEN);
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        tokenService.invalidateToken(request);
+
+        return ResponseEntity.ok().build();
     }
 
 }
