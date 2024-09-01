49c155c567f359fa60a4471ccb2eb574c1db664f
Carlos Eduardo Bahia da Silva Araújo <104322621+Carlos-Bahia@users.noreply.github.com>
2024-09-01 12:06:17 -0300

    Hotfix para ignorar canpos não utilizados

--- src/main/java/br/edu/ufape/gobarber/dto/barber/BarberCreateDTO.java
+++ src/main/java/br/edu/ufape/gobarber/dto/barber/BarberCreateDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Setter
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BarberCreateDTO {
 
     private String email;
